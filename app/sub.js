c6e15c6a21b2a228e1b8e20cdd2c4dcd0c2d7ce4
nickleefly <nickleefly@gmail.com>
2015-12-09 14:51:34 +0800

    add babel loader

--- app/sub.js
+++ app/sub.js
@@ -1,7 +1,5 @@
-function generateText() {
+export default function() {
   var element = document.createElement('h2');
-  element.innerHTML = "Hello h2 world";
+  element.innerHTML = "Hello h2 world hahaha";
   return element;
 }
-
-module.exports = generateText;
